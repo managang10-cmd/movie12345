47d06159721faf54aad145ebbd1b2e042dced20d
managang10-cmd <managang10@gmail.com>
2026-09-13 22:22:39 +0530

    Update CHECK_DATE to new value

--- tracker.py
+++ tracker.py
@@ -4,7 +4,7 @@ from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
 # ── CONFIGURATION ────────────────────────
-CHECK_DATE  = "20260809"
+CHECK_DATE  = "20260916"
 
 THEATRES = [
     {
