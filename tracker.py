058964bd5e09517b626bdd5504b12769d57ab7df
managang10-cmd <managang10@gmail.com>
2026-09-16 20:10:30 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -4,7 +4,7 @@ from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
 # ── CONFIGURATION ────────────────────────
-CHECK_DATE  = "20260924"
+CHECK_DATE  = "20260916"
 
 THEATRES = [
     {
